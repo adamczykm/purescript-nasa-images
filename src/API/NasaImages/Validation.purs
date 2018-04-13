99b643a9192ed2e05e000d806322bdbbdee10670
Maciej Buszka <maciej@buszka.eu>
2018-04-13 13:45:51 +0200

    Working validation

--- src/API/NasaImages/Validation.purs
+++ src/API/NasaImages/Validation.purs
@@ -3,32 +3,107 @@ module API.NasaImages.Validation where
 import Prelude
 
 import API.NasaImages (Metadata(..), SearchItem(..), SearchResult(..))
-import Control.Monad.Error.Class (throwError)
-import Data.Array (head)
-import Data.Foreign (F, Foreign, ForeignError(..), fail, readArray, readInt, readString)
-import Data.Foreign.Index (readProp)
-import Data.Traversable (traverse)
+import Data.Argonaut (Json, foldJson, toArray, toNumber, toObject, toString)
+import Data.Array (singleton, (!!))
+import Data.Bifunctor (lmap)
+import Data.Generic (class Generic, gShow)
+import Data.Int (fromNumber)
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Data.Monoid (class Monoid, mempty)
+import Data.Record.Fold (collect)
+import Data.StrMap (StrMap, lookup)
+import Data.Traversable (sequence, traverse)
+import Polyform.Validation (V(..), Validation, hoistFnMV, hoistFnV, runValidation)
 
-readMetadata :: Foreign -> F Metadata
-readMetadata f = do
-  th <- readProp "total_hits" f >>= readInt
-  pure $ Metadata { totalHits: th }
+data JsError = JsError (List String) String
+derive instance genericJsError :: Generic JsError
+instance showJsError :: Show JsError where show = gShow
 
-readSearchItem :: Foreign -> F SearchItem
-readSearchItem f = do
-  title <- readProp "title" f >>= readString
-  description <- readProp "description" f >>= readString
-  keywords <- readProp "keywords" f >>= readArray >>= traverse readString
-  nasaId <- readProp "nasa_id" f >>= readString
-  pure $ SearchItem { title, description, keywords, nasaId }
 
-readSearchResult :: Foreign -> F SearchResult
-readSearchResult f = do
-  href <- readProp "href" f >>= readString
-  metadata <- readProp "metadata" f >>= readMetadata
-  arr <- readProp "items" f >>= readArray
-  items <- arr # traverse (readProp "data" >=> readSearchItem)
-  pure $ SearchResult { href, metadata, items }
+type JsValidation m a = Validation m (Array JsError) Json a
+
+jsType :: Json -> String
+jsType = foldJson
+  (const "null")
+  (const "bool")
+  (const "number")
+  (const "string")
+  (const "array")
+  (const "object")
+
+fail :: forall a. String -> V (Array JsError) a
+fail msg = Invalid $ singleton $ JsError Nil msg
+
+int :: forall m. Monad m => JsValidation m Int
+int = hoistFnV $ \v ->
+  case toNumber v >>= fromNumber of
+    Nothing -> fail (jsType v <> " is not an int")
+    Just n -> pure n
+
+object :: forall m. Monad m => Validation m (Array JsError) Json (StrMap Json)
+object = hoistFnV $ \v ->
+  case toObject v of
+    Nothing -> fail (jsType v <> " is not an object")
+    Just o -> pure o
+
+string :: forall m. Monad m => JsValidation m String
+string = hoistFnV $ \v ->
+  case toString v of
+    Nothing -> fail (jsType v <> " is not a string")
+    Just s -> pure s
+
+field :: forall m a. Monad m => String -> JsValidation m a -> JsValidation m a
+field f nested = object >>> hoistFnMV (\v ->
+  case lookup f v of
+    Nothing -> pure $ fail ("no field " <> show f <> " in object " <> show v)
+    Just json -> do
+      res <- runValidation nested json
+      pure $ lmap (map \(JsError p e) -> JsError (f : p) e) res)
+
+optionalField
+  :: forall m a
+   . Monad m
+  => Monoid a
+  => String -> JsValidation m a -> JsValidation m a
+optionalField f nested = object >>> hoistFnMV (\v ->
+  case lookup f v of
+    Nothing -> pure $ pure mempty
+    Just json -> do
+      res <- runValidation nested json
+      pure $ lmap (map \(JsError p e) -> JsError (f : p) e) res)
+
+array :: forall m a. Monad m => JsValidation m (Array Json)
+array = hoistFnV $ \v ->
+  case toArray v of
+    Nothing -> fail (jsType v <> " is not an array")
+    Just a -> pure a
+
+metadata :: forall m. Monad m => JsValidation m Metadata
+metadata = Metadata <$> (collect { "totalHits": field "total_hits" int })
+
+elem :: forall m a. Monad m => Int -> JsValidation m a -> JsValidation m a
+elem i v = array >>> hoistFnMV (\arr ->
+  case arr !! i of
+    Nothing -> pure $ fail ("no element at index " <> show i)
+    Just a -> runValidation v a)
+
+arrayOf :: forall m a. Monad m => JsValidation m a -> JsValidation m (Array a)
+arrayOf v = array >>> hoistFnMV f
   where
-    srch [f] = readSearchItem f
-    srch _ = fail $ ForeignError "empty data field"
+    f = map sequence <<< traverse (runValidation v)
+
+searchItem :: forall m. Monad m => JsValidation m SearchItem
+searchItem = SearchItem <$> (collect
+  { title: field "title" string
+  , description: field "description" string
+  , keywords: optionalField "keywords" $ arrayOf string
+  , nasaId: field "nasa_id" string
+  })
+
+searchResult :: forall m. Monad m => JsValidation m SearchResult
+searchResult = SearchResult <$> (collect
+  { href: field "href" string
+  , items: field "items" $ arrayOf (field "data" (elem 0 searchItem))
+  , metadata: field "metadata" $ metadata
+  })
--- src/Main.purs
+++ src/Main.purs
@@ -3,28 +3,21 @@ module Main where
 import Prelude
 
 import API.NasaImages (SearchRequest(..), toUrlEncoded)
-import API.NasaImages.Validation (readSearchResult)
+import API.NasaImages.Validation (field, searchResult)
 import Control.Monad.Aff (launchAff)
-import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Aff.Console (logShow)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Except (runExcept, runExceptT)
 import Data.Either (Either(..))
-import Data.Foreign (Foreign)
-import Data.Foreign.Index (readProp)
 import Data.FormURLEncoded (encode)
 import Data.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..))
-import Data.Newtype (unwrap)
-import Debug.Trace (traceAny, traceAnyA)
-import Network.HTTP.Affjax (AJAX, Affjax, AffjaxRequest, affjax, defaultRequest)
+import Network.HTTP.Affjax (AffjaxRequest, affjax, defaultRequest)
+import Polyform.Validation (V(..), runValidation)
 
 buildRequest :: forall e. SearchRequest -> AffjaxRequest Unit
 buildRequest r = 
   let
     url = r # toUrlEncoded # encode
-  in traceAny url \_ -> defaultRequest { url = "https://images-api.nasa.gov/search?" <> url, method = Left GET }
+  in defaultRequest { url = "https://images-api.nasa.gov/search?" <> url, method = Left GET }
 
 example = SearchRequest
   { q : Just "apollo 11"
@@ -36,6 +29,7 @@ main :: _
 main = launchAff $ do
   resp <- affjax (buildRequest example)
   let f = resp.response
-  case runExcept $ readProp "collection" f >>= readSearchResult of
-    Left e -> logShow e
-    Right v -> logShow v
+  v <- runValidation (field "collection" searchResult) f
+  case v of
+    Invalid e -> logShow e
+    Valid _ v -> logShow v
