a91cb63b04e4816c5e1c7b6221bf6399d3f2fb5a
Maciej Buszka <maciej@buszka.eu>
2018-04-20 14:58:20 +0200

    Readme + move example to test

---
+++ README.md
@@ -0,0 +1,11 @@
+# purescript-nasa-api
+
+implements image api of Nasa images based on 
+[this document](https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf)
+
+ - `Methods.search` runs a search for specified request and returns a `Validation` of search result
+ - `Methods.retrieve` retrieves asset based on URL which is available in search item
+ - `Methods.searchAndRetrieve` combines above functions, providing validation which returns
+    search results with filled asset information
+
+Example usage in `test/Main.purs`
--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,24 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import API.NasaImages.Methods (searchAndRetrieve)
+import API.NasaImages.Search (Request(..))
+import Control.Monad.Aff (launchAff)
+import Control.Monad.Aff.Console (logShow)
+import Data.Maybe (Maybe(..))
+import Polyform.Validation (V(..), runValidation)
+
+example :: Request
+example = Request
+  { query : Just "apollo 11"
+  , description : Just "moon landing"
+  , keywords : []
+  }
+
+main :: _
+main = launchAff $ do
+  v <- runValidation searchAndRetrieve example
+  case v of
+    Invalid e -> logShow e
+    Valid _ r -> logShow r
